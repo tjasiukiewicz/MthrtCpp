32e205136c551d305cad3c4aa22415dc8eebe052
Tomasz Jasiukiewicz <t.jasiukiewicz@gmail.com>
2025-10-24 09:49:55 +0200

    ac complete

--- exercises/active_object/main.cpp
+++ exercises/active_object/main.cpp
@@ -4,15 +4,20 @@
 #include <mutex>
 #include <iostream>
 #include <functional>
+#include <chrono>
 
 class ActiveObject {
 public:
 	using submit_t = void (ActiveObject::*)(std::string);
 
-	ActiveObject() { // thread
+	ActiveObject() 
+		: stop{false} {
+		work_thread = std::thread(&ActiveObject::work_loop, this);
 	}
 
 	~ActiveObject() {
+		doStop();
+		work_thread.join();
 	}
 
 	void process(submit_t mthd, std::string msg) {
@@ -22,12 +27,46 @@ public:
 		work_queue.push(std::packaged_task<void(void)>(std::bind(mthd, this, msg)));
 	}
 
-	void work1(std::string msg) {}
-	void work2(std::string msg) {}
-	void work3(std::string msg) {}
+	void doStop() {
+		stop = true;
+	}
+
+	void work1(std::string msg) {
+		{
+			std::lock_guard<std::mutex> _(out_mutex);
+			std::cout << "Work1 start: " << msg << '\n';
+		}
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		{
+			std::lock_guard<std::mutex> _(out_mutex);
+			std::cout << "Work1 stop:  " << msg << '\n';
+		}
+	}
+	void work2(std::string msg) {
+		{
+			std::lock_guard<std::mutex> _(out_mutex);
+			std::cout << "Work2 start: " << msg << '\n';
+		}
+		std::this_thread::sleep_for(std::chrono::seconds(2));
+		{
+			std::lock_guard<std::mutex> _(out_mutex);
+			std::cout << "Work2 stop:  " << msg << '\n';
+		}
+	}
+	void work3(std::string msg) {
+		{
+			std::lock_guard<std::mutex> _(out_mutex);
+			std::cout << "Work3 start: " << msg << '\n';
+		}
+		std::this_thread::sleep_for(std::chrono::seconds(3));
+		{
+			std::lock_guard<std::mutex> _(out_mutex);
+			std::cout << "Work3 stop:  " << msg << '\n';
+		}
+	}
 private:
 	void work_loop() {
-		for(;;) {
+		while(! stop) {
 			std::unique_lock<std::mutex> lk(mtx);
 			if (not work_queue.empty()) {
 				auto task = std::move(work_queue.front());
@@ -41,11 +80,16 @@ private:
 	}
 	std::mutex mtx;
 	std::queue<std::packaged_task<void(void)>> work_queue;
+	std::mutex out_mutex;
+	std::thread work_thread;
+	bool stop;
 };
 
 int main() {
-	ActiveObject ac;
+	ActiveObject ac{};
 	ac.process(&ActiveObject::work1, "first task");
 	ac.process(&ActiveObject::work2, "second task");
-	ac.process(&ActiveObject::work1, "thrid task");
+	ac.process(&ActiveObject::work3, "thrid task");
+
+	std::this_thread::sleep_for(std::chrono::seconds(5));
 }
